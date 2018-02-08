4402da19840183ed251b58f9d0abd314f012f05f
darrenrector91 <darrenrector91@gmail.com>
2018-02-07 22:30:45 -0600

    working sortable/filterable angularjs table

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,12 +1,36 @@
-var app = angular.module('soloApp', ['ngRoute', 'ngMaterial'])
-.config(function($mdThemingProvider) {
-  $mdThemingProvider.theme('default')
-    .primaryPalette('blue')
-    .warnPalette('red')
-    .accentPalette('lime')
-    .backgroundPalette('grey')
-    .dark();
-});
+angular.module("myApp", ["ngTable"])
+  .controller('tableControl', tableControl);
+tableControl.$inject = ["NgTableParams"];
 
-// Available palettes: red, pink, purple, deep-purple, indigo, blue, light-blue, cyan, teal,
-// green, light-green, lime, yellow, amber, orange, deep-orange, brown, grey, blue-grey
+function tableControl(NgTableParams) {
+
+  var self = this;
+  var data = [{
+    date: "06/12/2017",
+    city: "Big Sky",
+    state: "MT",
+    species: "Rainbow trout",
+    rod: "Temple Fork",
+    reel: "Sage 2200",
+    tackle_bait: "Stone fly",
+    body_of_water: "Madison River",
+    image_url: "images/brownTrout.jpg"
+  },
+  {
+    date: "07/12/2016",
+    city: "Bozeman",
+    state: "MT",
+    species: "Brown trout",
+    rod: "Temple Fork",
+    reel: "Sage 2200",
+    tackle_bait: "Grasshopper",
+    body_of_water: "Gallatin River",
+    image_url: "http://bit.ly/2nK952F"
+  }, /*,*/ ];
+  self.tableParams = new NgTableParams({
+    page: 1, // show first page
+    count: 10 // count per page
+  }, {
+    dataset: data
+  });
+}
